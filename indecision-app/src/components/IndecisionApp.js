621cdce41dac25a11d0a9109f3390d4e67b2c472
teja <tejareddy.teju@gmail.com>
2020-11-18 14:51:15 +0530

    update - setting up react model

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -4,13 +4,18 @@ import AddOption from './AddOption';
 import Header    from './Header';
 import Action    from './Action';
 import Options   from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
 	state = {
-		options: []
+		options: [],
+		selectedOption: undefined
 	};
 	handleDeleteOptions = () => {
 		this.setState(() => ({ options: [] }))
+	};
+	handleClearSelectedOption = () => {
+		this.setState(() => ({ selectedOption: undefined }));
 	};
 	handleDeleteOption = (optionToRemove) => {
 		this.setState((prevState) => ({
@@ -19,7 +24,10 @@ export default class IndecisionApp extends React.Component {
 	};
 	handlePick = () => {
 		const count = Math.floor(Math.random() * this.state.options.length);
-		alert(this.state.options[count]);
+		const option = this.state.options[count];
+		this.setState(() => ({
+			selectedOption: option
+		}));
 	};
 	handleAddOption = (option) => {
 		if (!option) {
@@ -69,6 +77,10 @@ export default class IndecisionApp extends React.Component {
 				/>
 				<AddOption
 					handleAddOption={this.handleAddOption}
+				/>
+				<OptionModal
+					selectedOption={this.state.selectedOption}
+					handleClearSelectedOption={this.handleClearSelectedOption}
 				/>
 			</div>
 		);
