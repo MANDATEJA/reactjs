1e063640bc29ece79f920b32c0e9a4ad7e6bc913
teja <tejareddy.teju@gmail.com>
2020-11-18 14:20:55 +0530

    update - passing children to component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp /> , document.getElementById('app'));
+const Layout = (props) => {
+  return (
+    <div>
+      <p>Header</p>
+      {props.children}
+      <p>Footer</p>
+    </div>
+  );
+}
+
+ReactDOM.render((
+  <Layout>
+    <h1>Page title</h1>
+    <p>This is my page</p>
+  </Layout>
+), document.getElementById('app'));
